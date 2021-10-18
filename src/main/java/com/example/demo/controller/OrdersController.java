0b11fddbd9bb0af5617b341afc1d9e90bc7e2d58
edmwat <edmwat@gmail.com>
2021-10-18 11:02:29 +0300

    added getOrderList method to ordersController

--- src/main/java/com/example/demo/controller/OrdersController.java
+++ src/main/java/com/example/demo/controller/OrdersController.java
@@ -1,12 +1,15 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController  
 @RequestMapping(value="/api")
 public class OrdersController {
+
 	public void postOrder() {
+
 	}
 
 }
